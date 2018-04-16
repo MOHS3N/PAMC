cdacc103a308f1fe84862c926ae070857c6827a9
Mohsen Niyazdel <mohsenniyazdel@gmail.com>
2018-04-16 22:09:40 +0430

    swich to PyCharm

--- hello.py
+++ hello.py
@@ -1,2 +1,13 @@
 import time
+import os
+
+
 print("Time is :" + time.ctime())
+print(os.getcwd())
+totalTime = 3
+curentTime = 0
+
+while(curentTime <= totalTime):
+    time.sleep(5)
+    print(curentTime)
+    curentTime = curentTime + 1
